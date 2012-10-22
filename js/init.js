ca10fbfd856baed14a9a085bcc208939fda6d179
Giulio Dellorbo <egm0121@gmail.com>
2012-10-22 12:49:48 +0300

    remove debug info

--- js/init.js
+++ js/init.js
@@ -1,7 +1,6 @@
 (function(global){
 var conf = upCloo.widgets;
 	upCloo.utils.ready(function(){
-		console.log('sdk loaded once');
 		var widgets = upCloo.utils.$byClass('upcloo-suggest-widget');
 		for(var i=0; i < widgets.length; i++){
 			upCloo.instances.push(new upCloo.suggest(conf[i].siteKey,conf[i].pageId,conf[i].vSiteKey,conf[i].opts,widgets[i]));
