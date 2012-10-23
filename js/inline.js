f73f23da5a8b843083455d11e3db569a106fdb80
egm0121 <egm0121@gmail.com>
2012-10-24 00:20:38 +0200

    new theme options / image option

--- js/inline.js
+++ js/inline.js
@@ -5,36 +5,47 @@
 		
 			this.data = false;
 			this.options = {};
-			this.widgetElem = elem;
-			
+			this.hasImage =false;
+			this.widgetElem = elem;	
 		};
 	Inline.prototype = {
 		'setOptions' :function(opts){
-		
 			this.options = opts || {};
 		},
 		'setData' : function(dataObj){
 			this.data = dataObj;
 		},
-		'_makeLink':function(obj){ return '<a href="'+obj.url+'">'+obj.title+'</a>';},
+		'_makeLink':function(obj){ 
+			
+			var link = document.createElement('a');
+				link.setAttribute('href',obj.url),
+				hasImg = 'image' in obj && obj.image.length > 0;
+				
+			link.innerHTML = hasImg ? "<img src='"+obj.image+"' alt='' border='0'/>"+obj.title : obj.title ;
+			upCloo.utils.bind(link,'mousedown',function(){this.setAttribute('href',obj.trackUrl);});
+			return link;
+		},
+		'setHasImage':function(yesno){
+			this.hasImage = yesno;
+		},
 		'render' : function(){
 			var arr = this.data,
-				tmpHeadline = document.createElement('span'),	
+				tmpHeadline = document.createElement('li'),	
 				tmpUl = document.createElement('ul'),
 				count = 'limit' in this.options ? parseInt(this.options.limit,10) : 3 ;
 				
 			upCloo.utils.addClass(this.widgetElem,'upcloo-inline');
-			
-			if('headline' in this.options ){
+			//has thumb 
+			if(this.hasImage)upCloo.utils.addClass(this.widgetElem,'upcloo-img');
+			if(this.options.headline ){
 				tmpHeadline.innerHTML = this.options.headline;
-				upCloo.utils.addClass(tmpHeadline,'upcloo-over-title');
+				upCloo.utils.addClass(tmpHeadline,'upcloo-inline-title');
 				this.widgetElem.appendChild(tmpHeadline);
 			}
 			for(var i=0; i < count; i++){
 				if(arr[i] === undefined)break;
 				var tmpLi = document.createElement('li');
-					upCloo.utils.addClass(tmpLi,'upcloo-suggest_li');
-					tmpLi.innerHTML = this._makeLink(arr[i]);
+					tmpLi.appendChild(this._makeLink(arr[i]));
 					tmpUl.appendChild(tmpLi);
 			}
 			this.widgetElem.appendChild(tmpUl);
