5ce5eb2a5a2b74f88e154263905af0a14c367882
egm0121 <egm0121@gmail.com>
2012-10-24 00:21:24 +0200

    updated classes

--- js/popover.js
+++ js/popover.js
@@ -6,6 +6,7 @@
 			this.data = false;
 			this.options = {};
 			this.widgetElem = document.createElement('div');
+			this.hasImage = false;
 			this.widgetElemInDom = false;
 		};
 	popOver.prototype = {
@@ -15,13 +16,24 @@
 		'setData' : function(dataObj){
 			this.data = dataObj;
 		},
-		'_makeLink':function(obj){ return '<a href="'+obj.url+'">'+obj.title+'</a>';},
+		'_makeLink':function(obj){ 
+			
+			var link = document.createElement('a');
+				link.setAttribute('href',obj.url),
+				hasImg = 'image' in obj && obj.image.length > 0;
+			link.innerHTML = hasImg ? "<img src='"+obj.image+"' alt='' border='0'/>"+obj.title : obj.title ;
+			upCloo.utils.bind(link,'mousedown',function(){this.setAttribute('href',obj.trackUrl);});
+			return link;
+		},
 		'_doScrollCheck': function(){
 			var that = this;
 			upCloo.utils.bind(window,'scroll',function(e) {
 				var nVScroll = document.documentElement.scrollTop || document.body.scrollTop;
-				nVScroll > 400 ? that.show() : that.hide();
+				nVScroll > 400 ? that.show() : that.hide() ;
 			});
+		},
+		'hidden': function(){
+			return this.widgetElem.style.display == 'none';
 		},
 		'show': function(){
 			this.widgetElem.style.display = 'block';
@@ -29,30 +41,35 @@
 		'hide': function(){
 			this.widgetElem.style.display = 'none';
 		},
+		'setHasImage':function(yesno){
+			this.hasImage = yesno;
+		},
 		'render' : function(){
-		
 			var arr = this.data,
 				tmpRoot = this.widgetElem,
 				tmpHeadline = document.createElement('li'),	
 				tmpUl = document.createElement('ul'),
 				count = 'limit' in this.options ? parseInt(this.options.limit,10) : 3 ;
 			upCloo.utils.addClass(tmpRoot,'upcloo-over');
+			//pos
 			upCloo.utils.addClass(tmpRoot,'upcloo-over-' + ('pos' in this.options ? this.options.pos : 'br'));
-			
+			//theme
+			upCloo.utils.addClass(tmpRoot,'upcloo-over-' + this.options.theme);
+			//headline
+			upCloo.utils.addClass(tmpHeadline,'upcloo-over-title');
+			if(this.hasImage)upCloo.utils.addClass(this.widgetElem,'upcloo-img');
 			if('headline' in this.options && this.options.headline ){
 				tmpHeadline.innerHTML = this.options.headline;
-				upCloo.utils.addClass(tmpHeadline,'upcloo-over-title');
 				tmpRoot.appendChild(tmpHeadline);
 			}
 			for(var i=0; i < count; i++){
 				if(arr[i] === undefined)break;
 				var tmpLi = document.createElement('li');
-					upCloo.utils.addClass(tmpLi,'upcloo-suggest_li');
-					tmpLi.innerHTML = this._makeLink(arr[i]);
+				
+					tmpLi.appendChild(this._makeLink(arr[i]));
 					tmpUl.appendChild(tmpLi);
 			}
 			tmpRoot.appendChild(tmpUl);
-			
 			if(!this.widgetElemInDom){
 				document.getElementsByTagName('body')[0].appendChild(this.widgetElem);
 				this.widgetElemInDom = true;
