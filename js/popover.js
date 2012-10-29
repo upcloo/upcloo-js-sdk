8ffc1852a474028620e8c5a523311ba24db9af46
egm0121 <egm0121@gmail.com>
2012-10-29 23:34:41 +0100

    added def image && close

--- js/popover.js
+++ js/popover.js
@@ -20,29 +20,52 @@
 			
 			var link = document.createElement('a');
 				link.setAttribute('href',obj.url),
-				hasImg = 'image' in obj  && this.hasImage && obj.image.length > 0;
-			link.innerHTML = hasImg ? "<img src='"+obj.image+"' alt='' border='0'/>"+obj.title : obj.title ;
+				imageSrc = obj.image.length > 0 ? obj.image : "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+			link.innerHTML = this.hasImg ? "<img src='"+imageSrc"' alt='' border='0'/>"+obj.title : obj.title ;
 			upCloo.utils.bind(link,'mousedown',function(){this.setAttribute('href',obj.trackUrl);});
 			return link;
 		},
 		'_doScrollCheck': function(){
+			
 			var that = this;
-			upCloo.utils.bind(window,'scroll',function(e) {
+			var handle = function(e) {
+				
 				var nVScroll = document.documentElement.scrollTop || document.body.scrollTop;
 				nVScroll > 400 ? that.show() : false;
 				nVScroll < 100 ? that.hide() : false;
-			});
+			};
+			that.refScrollHandler = handle;
+			upCloo.utils.bind(window,'scroll',handle);
+			
 		},
 		'hidden': function(){
 			return this.widgetElem.style.display == 'none';
 		},
 		'show': function(){
 			
+			var that = this;
+			if(!this.isAnimate && this.hidden()){
+				this.isAnimate = true;
+				var opacity = 0;
+				setTimeout(function me(){
+					opacity += 0.1;
+					that.widgetElem.style.opacity = ""+ opacity;
+					that.widgetElem.style.filter = 'alpha(opacity='+opacity*100+')'
+					if(opacity < 1){
+						setTimeout(me,50)
+					} else{
+						that.isAnimate = false;
+						opacity = 0;
+					}
+				},50)
+			}
+			
 			this.widgetElem.style.display = 'block';
 		},
 		'hide': function(){
-
-			console.log('hide');
+			
+			this.widgetElem.style.opacity = 0;
+			this.widgetElem.style.filter = 'alpha(opacity=0)'
 			this.widgetElem.style.display = 'none';
 		},
 		'setHasImage':function(yesno){
@@ -50,8 +73,10 @@
 		},
 		'render' : function(){
 			var arr = this.data,
+				that = this,
 				tmpRoot = this.widgetElem,
-				tmpHeadline = document.createElement('li'),	
+				tmpHeadline = document.createElement('li'),
+				closeBtn = document.createElement('span'),
 				tmpUl = document.createElement('ul'),
 				count = 'limit' in this.options ? parseInt(this.options.limit,10) : 3 ;
 			upCloo.utils.addClass(tmpRoot,'upcloo-over');
@@ -64,7 +89,14 @@
 			upCloo.utils.addClass(tmpHeadline,'upcloo-over-title');
 			if(this.hasImage)upCloo.utils.addClass(this.widgetElem,'upcloo-img');
 			if('headline' in this.options && this.options.headline ){
+				closeBtn.innerHTML = 'x';
+				upCloo.utils.bind(closeBtn,'click',function(){
+					upCloo.utils.unbind(window,'scroll',that.refScrollHandler)
+					tmpRoot.parentNode.removeChild(tmpRoot)
+				});
+				upCloo.utils.addClass(closeBtn,'upcloo-over-close');
 				tmpHeadline.innerHTML = this.options.headline;
+				tmpHeadline.appendChild(closeBtn);
 				tmpUl.appendChild(tmpHeadline);
 			}
 			for(var i=0; i < count; i++){
